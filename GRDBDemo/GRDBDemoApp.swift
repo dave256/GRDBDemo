439728cbe4cdcf677d139a1f1c12b6286a171ffe
Dave Reed <dave256@mac.com>
2023-04-02 13:54:09 -0400

    Initial Commit

---
+++ GRDBDemo/GRDBDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GRDBDemoApp.swift
+//  GRDBDemo
+//
+//  Created by David Reed on 4/2/23.
+//
+
+import SwiftUI
+
+@main
+struct GRDBDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
